f97e52fbd4690a88fa64a4a631ce49ccf74acb43
adripego <adrian.pena@WKSALMPBB438.u-tad.es>
2014-12-09 21:58:37 +0100

    primitivas y botes

--- Prototipado/main.cpp
+++ Prototipado/main.cpp
@@ -1,81 +1,81 @@
 //#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 
 #include "include/u-gine.h"
+#include <conio.h>
 
-
-void basica();
-void avanzada();
+void prototype();
 
 int main(int argc, char* argv[]) {
     Screen::Instance().Open(800, 800, false);
-    //basica();
-    avanzada();
+    prototype();
 	return 0;
 }
 
-void basica()
+void prototype()
 {
     int screenWidth = Screen::Instance().GetWidth();
     int screenHeight = Screen::Instance().GetHeight();
-    Font *font = new Font("data/monospaced.png");
-    String *text = new String("Hola, mundo");
-    double posX, posY, velX, velY;
-    velX = rand() % 127 + 128;
-    velY = rand() % 127 + 128;
-    //velX = 128 + (rand() % static_cast<int>(255 - 128 + 1));
-    //velY = 128 + (rand() % static_cast<int>(255 - 128 + 1));
-    
-    posX = Screen::Instance().GetWidth() / 2;
-    posY = Screen::Instance().GetHeight() / 2;
-    while (Screen::Instance().IsOpened() && !Screen::Instance().KeyPressed(GLFW_KEY_ESC)) {
-        Screen::Instance().Clear();
-        posX += velX * Screen::Instance().ElapsedTime();
-        posY += velY * Screen::Instance().ElapsedTime();
+    int iPosXChar = screenWidth / 2;
+    int iPosYChar = screenHeight - 80;
+    float posXfoe1 = screenWidth / 2;
+    float posYfoe1 = screenHeight / 2;
+    int velY = 10;
+    int velX = velY / 2;
+    int bounces = 1;
+    int sizeChar = 1;
 
-        if ((posX + font->GetTextWidth(*text))  > Screen::Instance().GetWidth())
-        {
-           posX = Screen::Instance().GetWidth() - font->GetTextWidth(*text);
-            velX *= -1;
-            Renderer::Instance().SetColor(255 + rand() % (255 + 1), 255 + rand() % (255 + 1), 255 + rand() % (255 + 1), 255);
-        }
-
-        if (posX < 0)
-        {
-            posX = 0;
-            velX *= -1;
-            Renderer::Instance().SetColor(255 + rand() % (255 + 1), 255 + rand() % (255 + 1), 255 + rand() % (255 + 1), 255);
-        }
-
-        if ((posY + font->GetTextHeight(*text)) > Screen::Instance().GetHeight())
-        {
-            posY = Screen::Instance().GetHeight() - font->GetTextHeight(*text);
-            velY *= -1;
-            Renderer::Instance().SetColor(255 + rand() % (255 + 1), 255 + rand() % (255 + 1), 255 + rand() % (255 + 1), 255);
-        }
-
-        if (posY < 0)
-        {
-            posY = 0;
-            velY *= -1;
-            Renderer::Instance().SetColor(255 + rand() % (255 + 1), 255 + rand() % (255 + 1), 255 + rand() % (255 + 1), 255);
-        }
-        font->Render(*text, posX, posY);
-        // Refrescamos la pantalla
-        Screen::Instance().Refresh();
-    }
-    delete text;
-    ResourceManager::Instance().FreeResources();
-}
-
-void avanzada()
-{
-    Font * font = new Font("data/arial16.png");
-    String *text = new String("Hola, mundo");
     while (Screen::Instance().IsOpened() && !Screen::Instance().KeyPressed(GLFW_KEY_ESC)) {
         Screen::Instance().Clear();
 
-        font->Render(*text,0,0);
+
+        if (Screen::Instance().KeyPressed('A'))
+        {
+            if (iPosXChar > 0)
+                iPosXChar -= 5; 
+        }
+            
+        if (Screen::Instance().KeyPressed('D'))
+        {
+            if (iPosXChar <= screenWidth)
+                iPosXChar += 5;
+        }
+
+
+       
+
+            posXfoe1 += velX;
+            posYfoe1 += velY;
+
+        if (posYfoe1 < 0.f)
+        {
+            posYfoe1 = 0.f;
+            velY *= -1;
+        }
+        else if (posYfoe1 > screenHeight)
+        {
+            posYfoe1 = screenHeight;
+            velY *= -1;
+
+        }
+        else if (posXfoe1 > screenWidth)
+        {
+            posXfoe1 = screenWidth;
+            velX *= -1;
+            bounces++;
+
+        }
+        else if (posXfoe1 < 0.f)
+        {
+            posXfoe1 = 1.f;
+            velX *= -1;
+        }
+ 
+        Screen::Instance().SetTitle(String::FromFloat(posYfoe1) + "posY | vely" + String::FromFloat(velY));
         
+        Renderer::Instance().SetColor(255, 255, 0, 255);
+        Renderer::Instance().DrawRect(iPosXChar,iPosYChar,20 * bounces,20);
+        Renderer::Instance().SetColor(255,0,0,255);
+        Renderer::Instance().DrawEllipse(posXfoe1, posYfoe1,30,30);
         Screen::Instance().Refresh();
     }
     ResourceManager::Instance().FreeResources();
